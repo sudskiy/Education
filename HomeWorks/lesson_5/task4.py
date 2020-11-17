15a4cfcac6bb6080d3f71d5399597d2cdfd83af7
sudskiy <bon-ger@mail.ru>
2020-11-17 21:45:56 +0300

    начало

--- HomeWorks/lesson_5/task4.py
+++ HomeWorks/lesson_5/task4.py
@@ -7,3 +7,11 @@
 # При этом английские числительные должны заменяться на русские.
 # Новый блок строк должен записываться в новый текстовый файл.
 
+slovar = {'One': 'один', 'Two': 'два', 'Three': 'три', 'Four': 'четыре'}
+
+with open('new_file_3.txt', 'r', encoding='utf-8') as start:
+    with open('new_new_file_3.txt', 'a', encoding='utf-8') as finish:
+        for el in start:
+            word = el[:el.index(' ')]
+            string = el.replace(word, slovar[word])
+            finish.write(string)
